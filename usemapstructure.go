96ee11197dabc8ca2937acc618f4dd17a51a6327
DouglasCAdolph <douglascadolph@gmail.com>
2018-09-20 01:23:49 -0500

    just a template for now really

---
+++ usemapstructure.go
@@ -0,0 +1,18 @@
+/*
+Mapstructure is a great repository. The tool can come in quite handy!
+
+usemapstructure.go is the source code related to the topics covered 
+on the Wiki page for DouglasCAdolph/Golang/Mapstructure. 
+
+Visit the Wiki for more details. 
+*/
+
+// import mapstructure here 
+
+/*
+Unknown fields: 
+
+You might find yourself working with unknown data that could map to 
+different structs. Mapstructure makes it easy. You can take your data, 
+and decode it, identifying what key-value pairs you've received.  
+*/
